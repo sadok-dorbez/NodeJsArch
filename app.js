991c98a8a9279a7da8378c80d35a357a69d16389
sadok-dorbez <sadokdorbez0@gmail.com>
2023-02-17 15:00:53 +0100

    Mongo DataBase

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
 const express=require('express');
 const http=require('http');
+const mongo=require('mongoose');
+const mongoconnection=require('./config/mongoconnection.json');
+
+mongo.connect(mongoconnection.url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DataBase Connected");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 var app=express();
 const UserRouter=require ("./routes/user");
