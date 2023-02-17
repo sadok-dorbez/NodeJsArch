341b0a70e447b89063055220c0699a3d41fea698
sadok-dorbez <sadokdorbez0@gmail.com>
2023-02-17 14:32:06 +0100

    show message

--- app.js
+++ app.js
@@ -2,5 +2,7 @@ const express=require('express');
 const http=require('http');
 
 var app=express();
+const UserRouter=require ("./routes/user");
+app.use("/user", UserRouter);
 const server=http.createServer(app);
 server.listen(3000,()=>console.log("server is running "));
