b89c9f2749161846759bab213ba9a3543c0dfcb8
sadok-dorbez <sadokdorbez0@gmail.com>
2023-02-24 14:40:08 +0100

    V3

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require('express');
 const http=require('http');
 const mongo=require('mongoose');
 const mongoconnection=require('./config/mongoconnection.json');
+const bodyParser = require("body-parser");
+const UserRouter=require ("./routes/user");
 
 mongo.connect(mongoconnection.url, {
     useNewUrlParser: true,
@@ -15,7 +17,10 @@ mongo.connect(mongoconnection.url, {
   });
 
 var app=express();
-const UserRouter=require ("./routes/user");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 app.use("/user", UserRouter);
 const server=http.createServer(app);
 server.listen(3000,()=>console.log("server is running "));
--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,90 @@
+const { urlencoded } = require('body-parser');
 const express=require('express');
-const router=express.Router();
+var router=express.Router();
+const User=require('../modele/user')
 
 router.get('/show',(req,res,next)=>{
-    res.send('Hello Sadok');
+    res.send('You are welcome');
+    
+});/*
+////////////////////////////////// Post By parameters
+router.get('/add/:name/:email/:cin',(req,res,next)=>{
+    console.log("notre data :"+JSON.stringify(req.params));
+    new user({
+        name:req.params.name,
+        email:req.params.email,
+        cin:req.params.cin
+    }).save((err,data)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log(data);
+        res.json(data);
+
+    });
+});
+*/
+router.post("/add", async function(req,res,next){
+    console.log("resultat:"+req.body);
+    try{
+
+        const user = new User({...req.body});
+        await user.save();
+        res.status(201).send("heyy");
+    } catch(err){
+        console.log(err);
+    }
+
 });
 
+///////////////////////////////////////// Post By Json BodyRequest
+
+router.post("/new",(req,res,next)=>{
+   // console.log("resultat:"+req.body);
+    try{
+
+        const User=new user(req.body);
+        User.save();
+        //res.json(req.body);
+        res.send("added successfully");
+    } catch(err){
+        console.log(err);
+    }
+   
+
+});
+
+/////////////////////////////////////// Get all list
+router.get('/getAll', async (req, res) => {
+    try{
+        const data = await user.find();
+        res.json(data)
+    }
+    catch(error){
+        res.status(500).json({message: error.message})
+    }
+})
+
+
+
+router.get("/getByCin/:cin",async(req,res,next)=>{
+    
+    
+    try{
+        tab=[];
+        const c=req.params.cin;
+        const data = await user.find();
+        if (c < 0){
+            throw new Error ("cin must be positive");
+          }
+          tab.push(data[c]);
+
+    }catch(error){
+        res.status(500).json({message:error.message})
+    }
+    return res.json(tab);
+})
+
+
+ 
 module.exports=router;
